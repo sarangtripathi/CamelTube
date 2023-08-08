cf91b9f78ca96731c4db08a60af675f5b5a2f990
unknown <sarangtripathi18@gmail.com>
2023-08-08 23:44:02 +0530

    third-commit

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { useParams} from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Videos, ChannelCard } from './'
+import { fetchFromApi } from '../utils/fetchFromApi'
+
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromApi(`channels?part="snippet&id=${id}`)
+  },[id])
   return (
     <div>ChannelDetail</div>
   )
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -9,7 +9,7 @@ import { demoThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannel
 const VideoCard = ({ video : {id:{videoId} , snippet} }) => { 
 
   return (
-    <Card sx={{ width:{ md: '300px',sm:'358px',xs: '100%'},boxShadow:'none' , borderRadius: 0 }}>
+    <Card sx={{ width:{ md: '320px',sm:'358px',xs: '100%'},boxShadow:'none' , borderRadius: 0 }}>
 
       <Link to={ videoId ? `/video/${videoId}` : demoVideoUrl } >
         <CardMedia 
