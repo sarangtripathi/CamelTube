822683ded13834d82c0d759825cb13fb4f4d6bdb
unknown <sarangtripathi18@gmail.com>
2023-08-09 23:52:58 +0530

    fourth_change

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -7,13 +7,32 @@ import { fetchFromApi } from '../utils/fetchFromApi'
 
 
 const ChannelDetail = () => {
-  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+    const { id } = useParams();
 
   useEffect(() => {
-    fetchFromApi(`channels?part="snippet&id=${id}`)
-  },[id])
+    fetchFromApi(`channels?part=snippet&id=${id}`)
+     .then((data) => setChannelDetail(data?.items[0]));
+
+     fetchFromApi(`search?part=snippet&id=${id}&part=snippet&order=date`)
+     .then((data) => setVideos(data?.items));
+
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+          height : '300px'
+        }}/>
+          <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+          <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+    </Box>
   )
 }
 
