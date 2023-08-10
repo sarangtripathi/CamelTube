a857826654d20df388b1b601fe5728c5c75162fc
unknown <sarangtripathi18@gmail.com>
2023-08-10 23:54:08 +0530

    fourth_change

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React , { useState , useEffect} from 'react';
+import { Box , Typography } from '@mui/material';
+import { Videos } from './';
+import { fetchFromApi } from '../utils/fetchFromApi';
+
 
 const SearchFeed = () => {
+
+
+  
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => { 
+    fetchFromApi(`search?part=snippet&q=`)
+      .then((data) => setVideos(data.items))
+  }, []);
+
   return (
-    <div>SearchFeed</div>
-  )
+
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh',flex: 2}} >
+        <Typography variant="h4" fontweight="bold" mb={2} sx={{color:'white'}}>
+            Search results for: <span style={{color: '#F31503'}}>
+            videos
+          </span>
+        </Typography>
+
+        <Videos videos={videos} />
+
+      </Box>
+      
+  );
 }
 
 export default SearchFeed
